67c85b1dfecbc00efda5a8da2dab8363d655c468
Katelyn Lindberg <katelynhouston@live.com>
2022-03-22 16:01:09 -0600

    setting up npm test for functions

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const subtract = function(a, b) {
-	return a - b;
+let array = [];
+
+//find new subtraction alg
+function subtract(array) {
+	return array.reduce((total, current) => total - current, 0);
 };
 
-const sum = function(array) {
+function sum(array) {
   return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(array) {
+function multiply(array) {
   return array.length
     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
     : 0;
 };
+
+function divide(array) {
+    return array.length
+    ? array.reduce((accumulator, nextItem) =>
+    accumulator / nextItem)
+    : 0;
+}
+
+//npm test code
+module.exports = {
+    subtract,
+    sum,
+    multiply,
+    divide
+  };
